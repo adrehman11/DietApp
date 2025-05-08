c9e4fddcaefcc4a5818e5109782b168bac00ac39
Abdul Rehman Sohail <adrehman11@gmail.com>
2025-05-08 12:13:52 +0500

    changes

--- models/message_model.js
+++ models/message_model.js
@@ -5,7 +5,7 @@ const MessageSchema = new mongoose.Schema({
     chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat", required: true }, // The chat this message belongs to
     sender: { type: mongoose.Schema.Types.ObjectId, required: true }, // Sender ID (Client or Coach)
     senderType: { type: String, enum: ["User", "Coach"], required: true }, // Identify sender type
-    text: { type: String, required: true }, // Message content
+    text: { type: String}, // Message content
     image: { type: String }, // Message content
     createdAt: { type: Date, default: Date.now }, // Timestamp for sorting messages
     isread: { type: Boolean, default: false }, // Read status
