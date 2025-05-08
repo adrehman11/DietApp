f84c5c7b4bce2c225b343458701e51607d5f3fec
Abdul Rehman Sohail <adrehman11@gmail.com>
2025-05-08 16:57:17 +0500

    changes

--- routes/staff_module/staff_routes.js
+++ routes/staff_module/staff_routes.js
@@ -14,7 +14,7 @@ const {
     changeSubscriptionTypeByClientId
   } = require("../../middlewares/index");
 
-  Router.post('/login',login,StaffController.login);
+  Router.post('/login',login,StaffController.login); 
   Router.post('/getStaffClientsByFilter',authorization([Roles.staff]),getStaffClientsByFilter,StaffController.getClientsByFilter);
   Router.post('/getStaffClientById',authorization([Roles.staff]),getClientById,StaffController.getClientById);
   Router.post('/changeSubscriptionStatusByClientId',authorization([Roles.staff]),changeSubscriptionStatusByClientId,StaffController.changeSubscriptionStatusByClientId);
